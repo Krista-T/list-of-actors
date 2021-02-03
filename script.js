41dd669aa06b9b24d3ca4c81a77ba4e5454eb25c
krista <krista.tomicic@gmail.com>
2021-02-03 12:25:59 +0100

    final commit

--- script.js
+++ script.js
@@ -3,37 +3,38 @@ document.querySelector("#modal").classList.add("hide");
 // fetch data locally
 function getActors() {
   fetch("./list/actors.json")
-  .then(res => res.json())
+    .then(res => res.json())
     .then(getData);
 }
 
-function getData(data){
+function getData(data) {
   console.log(data);
   data.forEach(actor => {
 
     const copy = document.querySelector("template").content.cloneNode(true);
     console.log(actor.fullname)
     //populate
-     copy.querySelector("h3").textContent = actor.fullname;
+    copy.querySelector("h3").textContent = actor.fullname;
 
-      //event for modal
-const modal = document.querySelector("#modal");
-const btn = copy.querySelector(".btn button");
-const close = document.querySelector(".close");
+    //event for modal
+    const modal = document.querySelector("#modal");
+    const btn = copy.querySelector(".btn button");
+    const close = document.querySelector(".close");
 
     btn.addEventListener("click", getMoreData);
+
     function getMoreData() {
-     modal.classList.remove("hide"); 
-     document.querySelector(".name").textContent = actor.fullname;
-     document.querySelector(".movie").textContent = "MOVIE:" +` ${actor.movie}`;
+      modal.classList.remove("hide");
+      document.querySelector(".name").textContent = actor.fullname;
+      document.querySelector(".movie").textContent = "MOVIE:" + ` ${actor.movie}`;
     }
 
-   close.addEventListener("click", hideModal);
-   function hideModal(){
-    modal.classList.add("hide"); 
-   } 
-     //append
- document.querySelector("main").appendChild(copy);
-  }); 
-}
+    close.addEventListener("click", hideModal);
 
+    function hideModal() {
+      modal.classList.add("hide");
+    }
+    //append
+    document.querySelector("main").appendChild(copy);
+  });
+}
